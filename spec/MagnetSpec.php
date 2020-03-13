5d2e751eb88e21d85f8ef3e4ca7f7c8d4c04d9d1
Adam Neale <adam@adam-neale.co.uk>
2020-03-13 14:58:30 +0000

    build magnet from torrent, add toString method

--- spec/MagnetSpec.php
+++ spec/MagnetSpec.php
@@ -2,14 +2,11 @@
 
 namespace spec\Amneale\Torrent;
 
+use Amneale\Torrent\Torrent;
 use PhpSpec\ObjectBehavior;
 
 class MagnetSpec extends ObjectBehavior
 {
-    private const URI = 'magnet:?xt=urn:btih:0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33' .
-        '&dn=Foo+bar+baz+torrent+file' .
-        '&tr=http%3A%2F%2Ffoo.bar.tracker%3A80' .
-        '&tr=udp%3A%2F%2Ffoo.bar.tracker%3A1234';
     private const HASH = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
     private const NAME = 'Foo bar baz torrent file';
     private const TRACKERS = [
@@ -17,12 +14,49 @@ class MagnetSpec extends ObjectBehavior
         'udp://foo.bar.tracker:1234',
     ];
 
-    public function it_can_be_built_from_a_uri(): void
+    private const RFC3986_URI = 'magnet:?xt=urn:btih:0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33' .
+        '&dn=Foo%20bar%20baz%20torrent%20file' .
+        '&tr=http://foo.bar.tracker:80' .
+        '&tr=udp://foo.bar.tracker:1234';
+
+    private const RFC1738_URI = 'magnet:?xt=urn:btih:0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33' .
+        '&dn=Foo+bar+baz+torrent+file' .
+        '&tr=http%3A%2F%2Ffoo.bar.tracker%3A80' .
+        '&tr=udp%3A%2F%2Ffoo.bar.tracker%3A1234';
+
+    public function it_can_be_built_from_an_RFC3986_uri(): void
     {
-        $this->beConstructedThrough('fromUri', [self::URI]);
+        $this->beConstructedThrough('fromUri', [self::RFC3986_URI]);
 
         $this->infoHash->shouldBe(self::HASH);
         $this->name->shouldBe(self::NAME);
         $this->trackers->shouldBe(self::TRACKERS);
+    }
+
+    public function it_can_be_built_from_an_RFC1738_uri(): void
+    {
+        $this->beConstructedThrough('fromUri', [self::RFC1738_URI]);
+
+        $this->infoHash->shouldBe(self::HASH);
+        $this->name->shouldBe(self::NAME);
+        $this->trackers->shouldBe(self::TRACKERS);
+    }
+
+    public function it_can_be_built_from_a_torrent(): void
+    {
+        $torrent = new Torrent(self::HASH, self::NAME, self::TRACKERS);
+
+        $this->beConstructedThrough('fromTorrent', [$torrent]);
+
+        $this->infoHash->shouldBe(self::HASH);
+        $this->name->shouldBe(self::NAME);
+        $this->trackers->shouldBe(self::TRACKERS);
+    }
+
+    public function it_can_be_represented_as_a_string(): void
+    {
+        $this->beConstructedWith(self::HASH, self::NAME, self::TRACKERS);
+
+        $this->__toString()->shouldReturn(self::RFC3986_URI);
     }
 }
--- src/Magnet.php
+++ src/Magnet.php
@@ -14,7 +14,7 @@ final class Magnet
     public ?string $name;
     public array $trackers;
 
-    private function __construct(string $infoHash, ?string $name = null, array $trackers = [])
+    public function __construct(string $infoHash, ?string $name = null, array $trackers = [])
     {
         $this->infoHash = $infoHash;
         $this->name = $name;
@@ -36,5 +36,25 @@ final class Magnet
         $trackers = $query->has('tr') ? $query->getAll('tr') : [];
 
         return new self($infoHash, $name, $trackers);
+    }
+
+    public static function fromTorrent(Torrent $torrent): self
+    {
+        return new self($torrent->infoHash, $torrent->name, $torrent->trackers);
+    }
+
+    public function __toString(): string
+    {
+        $query = Query::createFromParams(['xt' => 'urn:btih:' . $this->infoHash]);
+
+        if (!empty($this->name)) {
+            $query = $query->appendTo('dn', $this->name);
+        }
+
+        foreach ($this->trackers as $tracker) {
+            $query = $query->appendTo('tr', $tracker);
+        }
+
+        return (string) Uri::createFromString('magnet:')->withQuery($query);
     }
 }
